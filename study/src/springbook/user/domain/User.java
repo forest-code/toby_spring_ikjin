e4d3136f316c045c35e4bbc63e1c4f5f2ec85e85
ikjin <ikjinmansa@gmail.com>
2016-11-07 23:09:07 +0900

    최초등록

---
+++ study/src/springbook/user/domain/User.java
@@ -0,0 +1,29 @@
+package springbook.user.domain;
+
+public class User {
+	//VARCHAR(10) PK
+	String id;
+	//VARCHAR(20) Not null
+	String name;
+	//VARCHAR(20) Not null
+	String password;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
