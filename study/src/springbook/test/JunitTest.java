f908405887e7319925db566b2a9961e0e9ecb956
ikjin <ikjinmansa@gmail.com>
2016-11-20 16:24:34 +0900

    다시

---
+++ study/src/springbook/test/JunitTest.java
@@ -0,0 +1,64 @@
+package springbook.test;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+import static org.junit.matchers.JUnitMatchers.either;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/springbook/test/junit.xml")
+public class JunitTest {
+	@Autowired
+	ApplicationContext context;
+	
+	static Set<JunitTest> testObjects = new HashSet<JunitTest>();
+	static ApplicationContext contextObject = null;
+	
+	@Test
+	public void test1() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		
+		assertThat(contextObject == null || contextObject == this.context, is(true));
+		contextObject = this.context;
+	}
+	
+	@Test
+	public void test2() {
+//		assertThat(this, is(not(sameInstance(testObject))));
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		
+		assertTrue(contextObject == null || contextObject == this.context);
+		contextObject = this.context;
+	}
+	
+	@Test
+	public void test3() {
+//		assertThat(this, is(not(sameInstance(testObject))));
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		
+		assertThat(contextObject, either(is(nullValue())).or(is(this.contextObject)));
+		contextObject = this.context;
+	}
+	
+//	public static void main(String[] args) {
+//		JUnitCore.main("springbook.user.dao.UserDaoTest");
+//
+//	}
+
+}
