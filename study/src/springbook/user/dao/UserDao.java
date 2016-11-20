14e22f0a8ac2036ba327fbae220f0206ad22c4bb
ikjin <ikjinmansa@gmail.com>
2016-11-20 16:23:52 +0900

    2장 완료

--- study/src/springbook/user/dao/UserDao.java
+++ study/src/springbook/user/dao/UserDao.java
@@ -6,7 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import springbook.user.domain.User;
+
 import javax.sql.DataSource;
+
+import org.springframework.dao.EmptyResultDataAccessException;
 
 public class UserDao {
 	private DataSource dataSource;
@@ -36,17 +39,48 @@ public class UserDao {
 		ps.setString(1, id);
 		
 		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setId(rs.getString("name"));
-		user.setId(rs.getString("password"));
+		
+		User user = null;
+		if(rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setId(rs.getString("name"));
+			user.setId(rs.getString("password"));
+		}		
 		
 		rs.close();
 		ps.close();
 		c.close();
 		
+		if(user == null) throw new EmptyResultDataAccessException(1);
+		
 		return user;
+	}
+	
+	public void deleteAll() throws SQLException {
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("delete from users");
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public int getCount() throws SQLException {
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c. prepareStatement("select count(*) from users");
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return  count;
 	}
 	
 }
--- study/src/springbook/user/domain/User.java
+++ study/src/springbook/user/domain/User.java
@@ -8,6 +8,16 @@ public class User {
 	//VARCHAR(20) Not null
 	String password;
 	
+	public User(String id, String name, String password) {
+		this.id = id;
+		this.name =name;
+		this.password = password;
+	}
+	
+	public User() {
+		
+	}
+	
 	public String getId() {
 		return id;
 	}
