cbef744cc445ea13b30af375c0f23de87355f873
ikjin <ikjinmansa@gmail.com>
2016-11-26 00:22:11 +0900

    3장 중간저장

--- study/src/springbook/user/dao/UserDao.java
+++ study/src/springbook/user/dao/UserDao.java
@@ -13,23 +13,28 @@ import org.springframework.dao.EmptyResultDataAccessException;
 
 public class UserDao {
 	private DataSource dataSource;
+	private JdbcContext jdbcContext;
 	
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 	
-	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("insert into user(id, name, password) valuse (?,?,?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-		
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
+	public void setJdbcContext(JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
+	
+	public void add(final User user) throws ClassNotFoundException, SQLException {
+		this.jdbcContext.workWithStatementStrategy(
+			new StatementStrategy() {
+				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+					PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+					ps.setString(1, user.getId());
+					ps.setString(1, user.getName());
+					ps.setString(1, user.getPassword());
+					return ps;
+				}
+			}
+		);
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
@@ -58,13 +63,13 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("delete from users");
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
+		this.jdbcContext.workWithStatementStrategy(
+			new StatementStrategy() {
+				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+					return c.prepareStatement("delete from users");
+				}
+			}
+		);
 	}
 	
 	public int getCount() throws SQLException {
