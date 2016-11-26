21645c3c5994d0df2dcd80b8a21c0c691ef2dea1
ikjin <ikjin@ikjin-PC>
2016-11-26 15:28:49 +0900

    3장 ~queryForObject

--- study/src/springbook/user/dao/UserDao.java
+++ study/src/springbook/user/dao/UserDao.java
@@ -5,87 +5,65 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import springbook.user.domain.User;
-
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import springbook.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource;
-	private JdbcContext jdbcContext;
+	private JdbcTemplate jdbcTemplate;
 	
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		
 		this.dataSource = dataSource;
 	}
 	
-	public void setJdbcContext(JdbcContext jdbcContext) {
-		this.jdbcContext = jdbcContext;
-	}
-	
 	public void add(final User user) throws ClassNotFoundException, SQLException {
-		this.jdbcContext.workWithStatementStrategy(
-			new StatementStrategy() {
-				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-					PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-					ps.setString(1, user.getId());
-					ps.setString(1, user.getName());
-					ps.setString(1, user.getPassword());
-					return ps;
-				}
-			}
-		);
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
-		
-		ResultSet rs = ps.executeQuery();
-		
-		User user = null;
-		if(rs.next()) {
-			user = new User();
-			user.setId(rs.getString("id"));
-			user.setId(rs.getString("name"));
-			user.setId(rs.getString("password"));
-		}		
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		if(user == null) throw new EmptyResultDataAccessException(1);
-		
-		return user;
-	}
-	
-	public void deleteAll() throws SQLException {
-		this.jdbcContext.workWithStatementStrategy(
-			new StatementStrategy() {
-				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-					return c.prepareStatement("delete from users");
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", 
+			new Object[] {id},
+			new RowMapper<User>() {
+				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+					User user = new User();
+					user.setId(rs.getString("id"));
+					user.setName(rs.getString("name"));
+					user.setPassword(rs.getString("password"));
+					return user;
 				}
 			}
 		);
 	}
 	
+	public void deleteAll() throws SQLException {
+//		this.jdbcTemplate.update(
+//			new PreparedStatementCreator() {
+//				public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//					return con.prepareStatement("delete from users");					
+//				}
+//			}
+//		);
+		this.jdbcTemplate.update("delete from users");
+	}
+	
 	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c. prepareStatement("select count(*) from users");
-		
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		int count = rs.getInt(1);
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return  count;
+//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//				return con.prepareStatement("select count(*) from usres");
+//			}
+//		}, new ResultSetExtractor<Integer>() {
+//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+//				rs.next();
+//				return rs.getInt(1);
+//			}
+//		});
+		return this.jdbcTemplate.queryForInt("select count(*) from users");
 	}
 	
 }
